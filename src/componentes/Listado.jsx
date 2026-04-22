84603fd9358695468d060dd6d6de7aeefe47f9fe
Thiago Aguero <aguerothiago965@gmail.com>
2026-04-22 18:58:28 -0300

    saque los datos hardcodeados de Listado.jsx para que solo esten en App.jsx

--- src/componentes/Listado.jsx
+++ src/componentes/Listado.jsx
@@ -1,11 +1,6 @@
 import Targeta from "./Targeta"
-const personas=[
-    {documento:"123456", nombres:"Benjamin", apellidos:"Panicheli"},
-    {documento:"123456", nombres:"Oriana", apellidos:"Panicheli"},
-    {documento:"123456", nombres:"Venja", apellidos:"Mansolla"},
-]
 
-export default function Listado(){
+export default function Listado({personas}){
     return(<div>
         <h1>Componente listado</h1>
         <div className="Listado">
